ee432de9b43371751f5fe78244ce6919c77639d0
G19 OptZhang22 <2778525216@qq.com>
2024-05-08 11:53:10 +0800

    physical_fast2

--- src/observer/storage/trx/vacuous_trx.cpp
+++ src/observer/storage/trx/vacuous_trx.cpp
@@ -32,7 +32,7 @@ void VacuousTrxKit::all_trxes(std::vector<Trx *> &trxes) { return; }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-RC update_record(Table *table, Record &record, int offset, int len, Value &value) 
+RC VacuousTrx::update_record(Table *table, Record &record, int offset, int len, Value &value) 
 { return table->update_record(record, offset, len, value); }
 
 RC VacuousTrx::insert_record(Table *table, Record &record) { return table->insert_record(record); }
